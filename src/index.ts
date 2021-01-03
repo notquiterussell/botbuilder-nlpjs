8a132f5b40a0cd100b3c11dd664a44d9fc6f509f
Russell Geraghty <russell@rosesareblue.co.uk>
2021-01-03 15:41:25 +0000

    Adds working module entry. Adds tests for entity extraction

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Answer } from './answer';
+export { IntentAnalysis } from './intent';
+export { Classification } from './classification';
+export { EntityAnalysis } from './entity';
+export { SentimentAnalysis } from './sentiment';
+export { NlpjsEngine } from './engine';
